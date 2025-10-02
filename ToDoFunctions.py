c5a048b933d3667151789eb15d0e676c071e78de
jwhittingham-code <jwhittingham002@gmail.com>
2025-10-03 08:58:53 +1000

    Finished text menu and created function to view the todo list

---
+++ ToDoFunctions.py
@@ -0,0 +1,3 @@
+def ToDoView(dict):
+    for k,v in dict.items():
+        print(f"({k}) -  {v}")
--- ToDoList.py
+++ ToDoList.py
@@ -1,10 +1,12 @@
 #ToDoList
 #Goal - Add, Remove, View and exit program
+import ToDoFunctions
 #Variables
 toDo = {1:"Create to Do list"}
 Running = True
 
 #functions
+
 
 
 #Prgram loop
@@ -13,9 +15,18 @@ while Running:
     
     Menu = input("Add,View,Remove or Exit:")
     Menu = Menu.lower()
-    print(Menu)
+    
+    match Menu: 
+        case "exit":
+            Running = False
+        case "view":
+            ToDoFunctions.ToDoView(toDo)
+            
+        case "add":
+            #Create function to Add items.
+            continue
+        case "remove":
+            #Create function to remove items.
+            continue
 
-    if Menu == "exit":
-        Running = False
-        
     
